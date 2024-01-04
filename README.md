cdcecc50a532b1e93a6b03fc38aea3e52c605290
shininglove <officialshineslove@gmail.com>
2024-01-03 22:39:45 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Items to do:
+
+- Load anime from json into database
+- Or connect to database with anime already loaded into it
+- look for components to render the anime into a couple groups
+- add a rating system for each anime card A-F
+- potentially add pics from Anime API
---
+++ resources/js/Pages/HomePage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+</script>
+<template>
+    <Head title="Home" />
+    <section name="nav" class="min-w-0 w-screen h-screen bg-slate-100">
+        <div class="h-20">
+            <p class="bg-red-500 text-white font-bold h-10 p-2">Anime Grading</p>
+        </div>
+        <section name="content">
+            <div class="border-2 border-red-500 p-2">
+                First Series
+            </div>
+        </section>
+    </section>
+</template>
